290268f301afaaa3bc27a49708825fc76562aec0
hemanthdev <hemanth.dev.fe@gmail.com>
2025-01-03 00:11:05 +0530

    deployeee2

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ import dotenv from "dotenv"
 dotenv.config()
 const port=process.env.PORT||1800;
 const app=express()
+
+const allowedOrigins = ['http://localhost:1803', 'https://cute-marigold-5db910.netlify.app'];
 app.use(cors({
-    origin: 'http://localhost:1803',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 //CORS:   This setup is useful for developing and testing your application locally, 
 // ensuring that only specific origins and methods are permitted to interact with your server
 
