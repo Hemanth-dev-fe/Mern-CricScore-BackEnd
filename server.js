2767dc07d55932b076a085292e2935fed1ca72ec
hemanthdev <hemanth.dev.fe@gmail.com>
2025-01-03 11:50:25 +0530

    d10

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ dotenv.config();
 const port = process.env.PORT || 1801;
 const app = express();
 
-const allowedOrigins = ['http://localhost:1803', 'https://cricscore-hemanth.netlify.app/'];
+const allowedOrigins = ['http://localhost:1803', 'https://cricscore-hemanth.netlify.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
@@ -21,6 +21,26 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+// Handle preflight requests
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', req.headers.origin);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.send();
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello hemanth don't worry all will be for you ...");
+});
+
+app.use("/products", products1);
+app.use("/addproducts", addproducs);
+app.use("/auth", userAuth);
+
+app.listen(port, () => console.log("server running on port", port));
 
 // Handle preflight requests
 app.options('*', cors());
