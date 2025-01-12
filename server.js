23c2aa4ac8f45a90c3918df05e255481d264f3c2
hemanthdev <hemanth.dev.fe@gmail.com>
2025-01-12 18:10:30 +0530

    3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const port = process.env.PORT || 1801;
 const app = express();
 
-const allowedOrigins = ['http://localhost:1803/', 'https://cute-marigold-5db910.netlify.app'];
+const allowedOrigins = ['http://localhost:1803/', 'https://cute-marigold-5db910.netlify.app','https://cricscoredev.netlify.app'];
 
 app.use(cors({
   origin: function (origin, callback) {
