5963748ff603ccb8648c43a9ed3044be29025dd3
hemanthdev <hemanth.dev.fe@gmail.com>
2025-01-13 13:15:39 +0530

    23

--- models.js
+++ models.js
@@ -23,18 +23,23 @@ mongoose.model("userauthentications", userAuthSchema);
 //     return new Date(utcOffset + istOffset);
 //   };
 //   console.log(getISTDateTime())
-
+function getISTDateTimeString() {
+    const date = new Date();
+    const utcOffset = date.getTimezoneOffset() * 60000; // Offset in milliseconds
+    const istOffset = 5.5 * 60 * 60 * 1000; // IST offset in milliseconds
+    const istDate = new Date(date.getTime() + utcOffset + istOffset);
+    const timeValue = istDate.toLocaleTimeString('en-US', { hour12: false });
+    const dateValue = istDate.toLocaleDateString('en-US');
+    return `${dateValue} ${timeValue}`;
+  }
+  console.log(getISTDateTimeString())
 const QuizAuthenticationSchema=new mongoose.Schema(
     {
         userId:{type:mongoose.Schema.Types.ObjectId,ref:"userAuthentication",required:true},
         name:{type:String,required:true},
         email:{type:String,required:true},
         score:{type:Number,required:true},
-        date: { type: String, default: () => {
-            const timeValue = new Date().toLocaleTimeString();
-            const dateValue = new Date().toLocaleDateString();
-            return `${dateValue} ${timeValue}`;
-          }}
+        date: { type: String, default: getISTDateTimeString()}
     }
 )
 const QuizModel=
